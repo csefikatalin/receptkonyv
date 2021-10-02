074681c7eb74b5514473a838039f447e433b4fdf
Tanar <Tanar@oktatas.local>
2021-10-02 11:14:33 +0200

    let const,

--- receptkonyv.js
+++ receptkonyv.js
@@ -1,81 +1,84 @@
-var receptek = [];
-var sorID = 0; //itt tárolom,hogy melyik kép van éppen soron
-
+const receptek = [];
 $(function () {
-    //Ajax hívás és az adatok kiírása táblázatban
-    beolvas();
 
-    //A tr tag-ekre rátesszük a kattintás eseménykezelőt
-});
+  let sorID = 0; //itt tárolom,hogy melyik kép van éppen soron
+  //Ajax hívás és az adatok kiírása táblázatban
+  let fajlnev="etelek.json";
+  beolvas(fajlnev, receptek);
 
-function beolvas() {
+  //A tr tag-ekre rátesszük a kattintás eseménykezelőt
+
+  function beolvas(fajlnev, tomb) {
     $.ajax({
-        url: "etelek.json",
-        success: function (result) {
-            receptek = result.receptkonyv;
-            ajaxtablazat();
-        },
+      url: fajlnev,
+      success: function (result) {
+        tomb = result.receptkonyv;
+        ajaxtablazat(tomb);
+      },
     });
-}
+  }
 
-function ajaxtablazat() {
+  function ajaxtablazat(tomb) {
     /*táblázat létrehozása*/
-    console.log(receptek);
+    console.log(tomb);
     $("article").append("<table>");
 
     var txt =
-        "<tr id='fejlec'><th>Név</th><th>Elkészítési idő</th><th>Kép</th><th>leírás</th><th>Hozzávalók</th></tr>";
-    for (var i = 0; i < receptek.length; i++) {
-        txt += "<tr id='" + i + "'>";
-        for (var item in receptek[i]) {
-            txt += "<td>" + receptek[i][item] + "</td>";
+      "<tr id='fejlec'><th>Név</th><th>Elkészítési idő</th><th>Kép</th><th>leírás</th><th>Hozzávalók</th></tr>";
+      tomb.forEach(function(value, index){
+        txt += "<tr id='" + index + "'>";
+        for (let item in value) {
+          txt += "<td>" + value[item] + "</td>";
         }
         txt += "</tr>";
-    }
+      });
     $("article table").html(txt);
-    $("tr").on("click", etelKivalasztas);
+    $("tr").on("click",  etelKivalasztas);
     $("tr").hover(function () {
-        $(this).toggleClass("hatter");
+      $(this).toggleClass("hatter");
     });
-}
+  }
 
-function etelKivalasztas() {
+  function etelKivalasztas() {
     if ($(this).attr("id") === "fejlec") {
-        rendezes();
+      rendezes();
     } else {
-        sorID = Number($(this).attr("id"));
-        console.log(receptek[sorID].kep);
-        megjelenit();
+        console.log(receptek);
+      sorID = Number($(this).attr("id"));
+      console.log(sorID);
+      console.log(receptek[sorID]);
+      megjelenit();
     }
-}
+  }
 
-function balra() {
+  function balra() {
     sorID = sorID - 1;
     if (sorID < 0) {
-        sorID = receptek.length - 1;
+      sorID = receptek.length - 1;
     }
 
     megjelenit();
-}
-function jobbra() {
+  }
+  function jobbra() {
     sorID = sorID + 1;
     if (sorID > receptek.length - 1) {
-        sorID = 0;
+      sorID = 0;
     }
 
     megjelenit();
-}
+  }
 
-function megjelenit() {
+  function megjelenit() {
     $("#kep img").attr("src", receptek[sorID].kep);
-}
+  }
 
-function rendezes() {
+  function rendezes() {
     var mezo = "nev";
     receptek.sort(function (a, b) {
-        merre = Number(b[mezo] > a[mezo]) - 0.5;
+      merre = Number(b[mezo] > a[mezo]) - 0.5;
 
-        return merre;
+      return merre;
     });
     console.log(receptek);
-}
+  }
+});
